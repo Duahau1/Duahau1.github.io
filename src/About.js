82986bb45ccd526e31617cfd69ed25f5c3c4ae0f
vannguyen <vannguyen599@u.boisestate.edu>
2020-09-30 21:19:22 -0600

    My first working react app

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react';
+import Tweet from './tweet';
+function About (){
+
+  return (
+    <div>
+      <h1>About page</h1>
+
+    </div>
+  )
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React,{useState} from 'react';
+import Tweet from './tweet';
+import Nav from './nav';
+import About from './About';
+import Item from './itemdetail';
+import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+function App (){
+  const home =()=>{
+    return (
+    <div>
+      <h1>Home page</h1>
     </div>
-  );
+    )
+  }
+  return (
+    <Router>
+    <div>
+      <h1>This is my React page</h1>
+    
+      <Nav />
+      <Switch>
+      <Route path='/' exact component={home}/>
+      <Route path='/about' component={About}/>
+      <Route path='/tweet' exact component ={()=><Tweet name="QBT" message="Toi thich KXY" />}/>
+      <Route path="/tweet/:id" component={Item}/>
+      </Switch>
+    </div>
+    </Router>
+  )
 }
 
 export default App;
